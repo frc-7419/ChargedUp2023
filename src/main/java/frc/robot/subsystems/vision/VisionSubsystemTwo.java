2184a2696ae7de9bb3600b0a65ff5bc40e86ee9c
Kaival Shah <kaival.s.shah@gmail.com>
2023-01-08 11:08:56 -0800

    Update VisionSubsystemTwo.java
    Co-Authored-By: wlmchen <65417452+wlmchen@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystemTwo.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystemTwo.java
@@ -25,8 +25,6 @@ import edu.wpi.first.math.geometry.Translation3d;
 import java.util.Optional;
 
 public class VisionSubsystemTwo extends SubsystemBase {
-  // set up a new instance of NetworkTables (the api/library used to read values
-  // from limelight)
   private PhotonCamera cam1;
   private PhotonCamera cam2;
   private List<PhotonTrackedTarget> camTargets;
@@ -37,7 +35,8 @@ public class VisionSubsystemTwo extends SubsystemBase {
   private Map<String, Double> val2;
   private Optional<Pair<Pose3d, Double>> result;
 
-  // AprilTagFieldLayout aprilTagFieldLayout = new ApriltagFieldLayout(AprilTagFieldLayout.loadFromResource(AprilTagFields.k2022RapidReact.m_resourceFile));
+  AprilTagFieldLayout aprilTagFieldLayout = new ApriltagFieldLayout(AprilTagFieldLayout.loadFromResource(AprilTagFields.k2022RapidReact.m_resourceFile)); //update with chargedup field when merged to photonvision + wpilib and avaialble
+  
   
   public VisionSubsystemTwo() {
     cam1 = new PhotonCamera("teri_ma");
@@ -54,53 +53,5 @@ public class VisionSubsystemTwo extends SubsystemBase {
   @Override
   public void periodic() {
     result = robotPoseEstimator.update();
-    // PhotonPipelineResult[] results = { cam1.getLatestResult(), cam2.getLatestResult() };
-    // boolean[] hasTargets = new boolean[results.length];
-    // for (int i = 0; i < results.length; i+a+) {
-    //   hasTargets[i] = results[i].hasTargets();
-    // }
-    // SmartDashboard.putBoolean("Cam 1 Targets", hasTargets[0]);
-    // SmartDashboard.putBoolean("Cam 2 Targets", hasTargets[1]);
-
-    // camTargets = addTargets(results);
-    
-    // if (hasTargets[0] || hasTargets[1]) {
-    //   val1 = setValues(results[0]);
-    //   val2 = setValues(results[1]);
-    //   bestVal = (val1.get("area") > val2.get("area")) ? val1 : val2;
-    // }
   }
-
-  // public List<PhotonTrackedTarget> addTargets(PhotonPipelineResult[] results) {
-  //   cam1Targets = results[0].getTargets();
-  //   cam2Targets = results[1].getTargets();
-  //   for (PhotonTrackedTarget target : camTargets) {
-  //     camTargets.add(target);
-      
-  //   }
-  //   return camTargets;
-  // }
-  
-  // public Map<String, Double> setValues(PhotonPipelineResult results) {
-  //   Map<String, Double> values = new HashMap<String, Double>();
-  //   values.put("yaw", results.getBestTarget().getYaw());
-  //   values.put("pitch", results.getBestTarget().getPitch());
-  //   values.put("area", results.getBestTarget().getArea());
-  //   values.put("skew", results.getBestTarget().getSkew());
-  //   return values;
-  // }
-
-  // public double getYaw() {
-  //     return bestVal.get("yaw");
-  // }
-  // public double getPitch() {
-  //   return bestVal.get("pitch");
-  // }
-  // public double getArea() {
-  //   return bestVal.get("area");
-  // }
-  // public double getSkew() {
-  //   return bestVal.get("skew");
-  // }
-
 }
