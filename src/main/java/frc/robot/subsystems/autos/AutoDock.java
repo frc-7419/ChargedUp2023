018d52990fba9c060dca0498361e6f26e9a28bfe
kriiler <icicle.bear212@gmail.com>
2023-01-10 18:09:32 -0800

    Update AutoDock.java

---
+++ src/main/java/frc/robot/subsystems/autos/AutoDock.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drive.DriveBaseSubsystem;
+import frc.robot.subsystems.gyro.GyroSubsystem;
+
+public class AutoDock extends SequentialCommandGroup{
+    public AutoDock(DriveBaseSubsystem driveBaseSubsystem){
+
+    }
+}
