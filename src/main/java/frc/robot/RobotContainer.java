e67a1c8196b5d977f2e70025609a28697d21c975
QLS7419TechSupport <qls7419leadership@gmail.com>
2023-01-11 17:02:27 -0800

    smartdash vals

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.CvSink;
@@ -19,9 +20,10 @@ import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.robot.subsystems.elevator.MaintainElevatorPosition;
 import frc.robot.subsystems.elevator.RunElevatorWithJoystick;
 import frc.robot.subsystems.gyro.GyroSubsystem;
+// import frc.robot.subsystems.vision.PoseEstimateTwoSubsystem;
 import frc.robot.subsystems.vision.VisionSubsystem;
 
-public class RobotContainer {
+public class RobotContainer{
   private final XboxController joystick1 = new XboxController(0);
   private final XboxController joystick2 = new XboxController(1);
   private final GyroSubsystem gyroSubsystem = new GyroSubsystem();
@@ -29,13 +31,13 @@ public class RobotContainer {
   private final VisionSubsystem visionSubsystem = new VisionSubsystem();
   private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
   private final BeamBreakSubsystem beamBreakSubsystem = new BeamBreakSubsystem();
-
+  // private final PoseEstimateTwoSubsystem poseEstimateTwoSubsystem = new PoseEstimateTwoSubsystem();
 
   private final ArcadeDrive arcadeDrive = new ArcadeDrive(joystick1, driveBaseSubsystem, 0.2, 0.2);
   // private final SmartShoot smartShoot = new SmartShoot(shooterSubsystem, feederSubsystem, loaderSubsystem, visionSubsystem, beamBreakSubsystem);
   // auto
 
-  private SendableChooser<Command> autonChooser = new SendableChooser<>();
+  // private SendableChooser<Command> autonChooser = new SendableChooser<>();
 
   public RobotContainer() {
     configureButtonBindings();
@@ -51,13 +53,12 @@ public class RobotContainer {
   private void configureAutoSelector() {
     // autonChooser.setDefaultOption("two ball", mttdTwoBall);
     // autonChooser.addOption("three ball", mttdThreeBall);
-    SmartDashboard.putData(autonChooser);
+    // SmartDashboard.putData(autonChooser);
   }
 
   public Command getAutonomousCommand() {
-    return autonChooser.getSelected();
+    return new WaitCommand(5);
   }
-
   public void setDefaultCommands() {
     driveBaseSubsystem.setDefaultCommand(arcadeDrive);
   }
