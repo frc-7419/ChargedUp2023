25c93e49f6c98e4799706db9f2fe98a597bf2950
kriiler <icicle.bear212@gmail.com>
2023-02-08 16:58:09 -0800

    Delete AutoDock.java

--- src/main/java/frc/robot/subsystems/autos/AutoDock.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.autos;
-
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.drive.DriveBaseSubsystem;
-
-public class AutoDock extends SequentialCommandGroup {
-  public AutoDock(DriveBaseSubsystem driveBaseSubsystem) {}
-}
