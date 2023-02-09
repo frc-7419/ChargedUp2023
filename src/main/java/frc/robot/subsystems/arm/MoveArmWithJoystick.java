8f919a1ad1c0f4f4fb8c06255b819055886c3926
StephenXie <49465499+StephenXie@users.noreply.github.com>
2023-02-08 17:17:15 -0800

    Create MoveArmWithJoystick.java

---
+++ src/main/java/frc/robot/subsystems/arm/MoveArmWithJoystick.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+
+public class MoveArmWithJoystick extends CommandBase {
+
+  private ArmSubsystem armSubsystem;
+  private XboxController joystick;
+
+  public MoveArmWithJoystick(ArmSubsystem armSubsystem, XboxController joystick) {
+    this.armSubsystem = armSubsystem;
+    this.joystick = joystick;
+    addRequirements(armSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+    armSubsystem.coastMain();
+  }
+
+  @Override
+  public void execute() {
+    double mainPowerJoystick = Constants.ArmConstants.mainArmPowerCoefficient * joystick.getRightY();
+    double extendedPowerJoystick = Constants.ArmConstants.extendedArmPowerCoefficient * joystick.getLeftY();
+    armSubsystem.setMainPower(mainPowerJoystick);
+    armSubsystem.setExtendedPower(extendedPowerJoystick);
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
