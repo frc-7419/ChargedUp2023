d16898e95b8705b028a5bebf03ab8f5201b09ce0
Keshav Badrinath <45376658+Aqtion@users.noreply.github.com>
2023-02-09 13:59:35 -0800

    elev

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -20,8 +20,8 @@ public class ElevatorSubsystem extends SubsystemBase {
   private RelativeEncoder encoder;
 
   public ElevatorSubsystem() {
-    elevatorMaster = new CANSparkMax(CanIds.leftElevator.id, MotorType.kBrushless);
-    elevatorSlave = new CANSparkMax(CanIds.rightElevator.id, MotorType.kBrushless);
+    elevatorMaster = new CANSparkMax(CanIds.leftElevatorFalcon.id, MotorType.kBrushless);
+    elevatorSlave = new CANSparkMax(CanIds.rightElevatorFalcon.id, MotorType.kBrushless);
     encoder = elevatorMaster.getEncoder();
 
     elevatorMaster.setInverted(true);
