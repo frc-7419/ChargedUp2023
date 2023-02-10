46d12059abaf0777315f24e1be8efa4a9dbd5e71
kriiler <icicle.bear212@gmail.com>
2023-02-09 17:06:33 -0800

    Update GenerateTrajectory.java

--- src/main/java/frc/robot/subsystems/drive/GenerateTrajectory.java
+++ src/main/java/frc/robot/subsystems/drive/GenerateTrajectory.java
@@ -5,40 +5,47 @@ import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import frc.robot.Constants;
+import frc.robot.Constants.DriveConstants;
 
 public class GenerateTrajectory extends RamseteCommand {
-  private final DriveBaseSubsystem drivetrain;
+  private final DriveBaseSubsystem driveBaseSubsystem;
 
   // ignore the unused warning, this trajectory is used
   private final Trajectory trajectory;
 
-  public GenerateTrajectory(DriveBaseSubsystem drivetrain, Trajectory trajectory) {
+  /**
+   * Generates trajectory based on RamseteCommand class and desired trajectory path.
+   * @param driveBaseSubsystem Gets DriveBaseSubsystem class for West Coast drive.
+   * @param trajectory Trajectory class
+   */
+  public GenerateTrajectory(DriveBaseSubsystem driveBaseSubsystem, Trajectory trajectory) {
     super(
         trajectory,
-        drivetrain::getCtrlsPoseEstimate,
-        new RamseteController(Constants.DriveConstants.kRamseteB, Constants.DriveConstants.kRamseteZeta),
-        new SimpleMotorFeedforward(Constants.DriveConstants.ks, Constants.DriveConstants.kv, Constants.DriveConstants.ka),
-        Constants.DriveConstants.driveKinematics,
-        drivetrain::getWheelSpeeds,
-        new PIDController(Constants.DriveConstants.kPDriveVelocity, 0, 0),
-        new PIDController(Constants.DriveConstants.kPDriveVelocity, 0, 0),
-        drivetrain::tankDriveVolts,
-        drivetrain);
-    this.drivetrain = drivetrain;
+        driveBaseSubsystem::getCtrlsPoseEstimate,
+        new RamseteController(DriveConstants.kRamseteB, DriveConstants.kRamseteZeta),
+        new SimpleMotorFeedforward(DriveConstants.ks, DriveConstants.kv, DriveConstants.ka),
+        DriveConstants.kDriveKinematics,
+        driveBaseSubsystem::getWheelSpeeds,
+        new PIDController(DriveConstants.kPDriveVelocity, 0, 0),
+        new PIDController(DriveConstants.kPDriveVelocity, 0, 0),
+        driveBaseSubsystem::tankDriveVolts,
+        driveBaseSubsystem);
+    this.driveBaseSubsystem = driveBaseSubsystem;
     this.trajectory = trajectory;
   }
 
+  /**
+   * Resets odometry when called
+   */
   @Override
   public void initialize() {
-    drivetrain.resetOdometry(trajectory.getInitialPose());
+    driveBaseSubsystem.resetOdometry(trajectory.getInitialPose());
     super.initialize();
   }
 
   @Override
   public void end(boolean interrupted) {
-    drivetrain.stop();
-    // drivetrain.resetOdometry(trajectory.getInitialPose());
+    driveBaseSubsystem.stop();
     super.end(interrupted);
   }
 }
