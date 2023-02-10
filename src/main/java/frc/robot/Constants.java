46c111a362f03b2c9c1af2f7b1e04bde4fd5b6bb
kriiler <icicle.bear212@gmail.com>
2023-02-09 17:14:04 -0800

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,13 +5,10 @@
 package frc.robot;
 
 import com.pathplanner.lib.PathPoint;
-import com.pathplanner.lib.PathPoint;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
