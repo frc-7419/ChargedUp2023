86640e34534a67c69afd796a4db29c9f64f027b8
Keshav Badrinath <45376658+Aqtion@users.noreply.github.com>
2023-02-27 10:12:28 -0800

    abs encoder

--- src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
@@ -3,9 +3,12 @@ package frc.robot.subsystems.arm;
 import static frc.robot.Constants.*;
 
 import com.ctre.phoenix.sensors.PigeonIMU;
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ArmSubsystem extends SubsystemBase {
 
   private CANSparkMax mainArmMotor1;
+  private AbsoluteEncoder absoluteEncoder;
   // private CANSparkMax mainArmMotor2;
   private boolean homed;
   private double homePosition = 0;
@@ -23,11 +27,14 @@ public class ArmSubsystem extends SubsystemBase {
   public ArmSubsystem() {
     mainArmMotor1 =
         new CANSparkMax(CanIds.armMain1.id, MotorType.kBrushless); // ENCODER DOESNT WORK
+    absoluteEncoder = mainArmMotor1.getAbsoluteEncoder(Type.kDutyCycle);
     // mainArmMotor2 = new CANSparkMax(CanIds.armMain2.id, MotorType.kBrushless);
 
     magneticLimitSwitch = new DigitalInput(0); // port for now
 
     extendedGyro = new PigeonIMU(0);
+    
+    // absoluteEncoder.setPositionConversionFactor(2 * RobotConstants.mainArmGearRatio / 4096);
     configureMotorControllers();
   }
 
@@ -57,8 +64,7 @@ public class ArmSubsystem extends SubsystemBase {
    * @return The position of the main arm, in units of rotations.
    */
   public double getMainPosition() {
-    return mainArmMotor1.getEncoder().getPosition()
-        - homePosition; // main arm motor 2's encoder works
+      return absoluteEncoder.getPosition();
   }
 
   /** Sets the home position variable to the current position of the main arm. */
@@ -111,15 +117,16 @@ public class ArmSubsystem extends SubsystemBase {
     // outputting arm positions to smart dashboard and homing status
     SmartDashboard.putNumber("Arm Position", getMainPosition());
     SmartDashboard.putNumber("Arm Position (2)", getMainPosition());
-    SmartDashboard.putNumber("Home Pos", homePosition);
-    SmartDashboard.putBoolean("Arm Homed", homed);
 
-    double[] gyroInformation = new double[3];
-    extendedGyro.getYawPitchRoll(gyroInformation);
+    // SmartDashboard.putNumber("Home Pos", homePosition);
+    // SmartDashboard.putBoolean("Arm Homed", homed);
 
-    // putting extended gyro values into smartdashboard
-    SmartDashboard.putNumber("Extended Yaw", gyroInformation[0]);
-    SmartDashboard.putNumber("Extended Pitch", gyroInformation[1]);
-    SmartDashboard.putNumber("Extended Roll", gyroInformation[2]);
+    // double[] gyroInformation = new double[3];
+    // extendedGyro.getYawPitchRoll(gyroInformation);
+
+    // // putting extended gyro values into smartdashboard
+    // SmartDashboard.putNumber("Extended Yaw", gyroInformation[0]);
+    // SmartDashboard.putNumber("Extended Pitch", gyroInformation[1]);
+    // SmartDashboard.putNumber("Extended Roll", gyroInformation[2]);
   }
 }
