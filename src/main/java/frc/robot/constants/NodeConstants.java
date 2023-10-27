fe752324f2d825adaa1771541e3ce29df4ab6b8a
Team7419 <126924979+Team7419@users.noreply.github.com>
2023-10-27 12:17:42 -0700

    Constants 2

--- src/main/java/frc/robot/constants/NodeConstants.java
+++ src/main/java/frc/robot/constants/NodeConstants.java
@@ -2,12 +2,12 @@ package frc.robot.constants;
 
 public final class NodeConstants {
   public static enum NodeState {
-    RESET(0.001, -1.81, 3.5),
-    GROUND(0.1, 20, 100),
+    RESET(0.001, 8.38, -41),
+    GROUND(-0.006, 20, 100),
     SUBSTATION(0.001, -60, 21),
     SINGLE_SUBSTATION(0.001, 50, 10.47),
     LOW(0, -184.26417230467044, -63),
-    HIGH(0.523760670146004, -202.97171677215192, -59.989710245916214);
+    HIGH(0.523760670146004, -202.97171677215192, -60.0);
 
     // public final double elevatorSetpoint;
     public final double armSetpoint;
