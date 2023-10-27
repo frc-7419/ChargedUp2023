36cfbdbec4df5328aa1495ba82e0aebdf3682565
Jolteous <wirelesskhurram@outlook.com>
2023-10-27 15:05:55 -0700

    AutoHighStop

--- src/main/java/frc/robot/commands/autos/AutoHighStop.java
+++ src/main/java/frc/robot/commands/autos/AutoHighStop.java
@@ -11,15 +11,20 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.actions.AutoScorePiece;
 import frc.robot.commands.actions.SmartRetract;
 import frc.robot.commands.actions.SmartRetractAuto;
+import frc.robot.constants.GripperConstants.GripperState;
 import frc.robot.constants.NodeConstants.NodeState;
 import frc.robot.subsystems.arm.ArmSubsystem;
+import frc.robot.subsystems.arm.ArmToSetpointWithFeedforward;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 import frc.robot.subsystems.drive.RunDrive;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
+import frc.robot.subsystems.elevator.ElevatorToSetpointWithFeedForward;
 import frc.robot.subsystems.gripper.GripperSubsystem;
+import frc.robot.subsystems.gripper.RunGripper;
 import frc.robot.subsystems.gyro.GyroSubsystem;
 import frc.robot.subsystems.gyro.SmartBalance;
 import frc.robot.subsystems.wrist.WristSubsystem;
+import frc.robot.subsystems.wrist.WristToSetpointWithFeedforward;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
@@ -29,9 +34,23 @@ public class AutoHighStop extends SequentialCommandGroup {
   public AutoHighStop(DriveBaseSubsystem driveBaseSubsystem, ElevatorSubsystem elevatorSubsystem, ArmSubsystem armSubsystem, WristSubsystem wristSubsystem, GripperSubsystem gripperSubsystem, GyroSubsystem gyroSubsystem, NodeState scoreLocation) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
+    
+      {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new AutoScorePiece(elevatorSubsystem, armSubsystem, wristSubsystem, gripperSubsystem, scoreLocation),
-      new SmartRetract(elevatorSubsystem, armSubsystem, wristSubsystem));
+        Commands.parallel(
+          new RunGripper(gripperSubsystem, GripperState.HOLD).raceWith(new WaitCommand(4)),
+        new ElevatorToSetpointWithFeedForward(elevatorSubsystem, scoreLocation).raceWith(new WaitCommand(2)),
+        new ArmToSetpointWithFeedforward(armSubsystem, scoreLocation).raceWith(new WaitCommand(2.5)),
+        new WristToSetpointWithFeedforward(wristSubsystem, armSubsystem, scoreLocation).raceWith(new WaitCommand(1))
+        ),
+        new RunGripper(gripperSubsystem, GripperState.SCORE).withTimeout(1),
+        new InstantCommand(gripperSubsystem::stop),
+    new SmartRetract(elevatorSubsystem, armSubsystem, wristSubsystem));
+
+  }
+      
         // // new RunDrive(driveBaseSubsystem, 0.28).withTimeout(2.8),
         // new SmartBalance(driveBaseSubsystem, gyroSubsystem),
         // new InstantCommand(driveBaseSubsystem::brake));
